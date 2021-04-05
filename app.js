56f744a66cbbfe763365f4803f95624b646446df
IvanRebora <ivan.rebora@hotmail.com>
2021-04-05 19:20:20 -0300

    Agregando procfile y el nuevo puerto

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const puerto = process.env.PORT
+
 
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.listen(3000, () =>{
+app.listen(puerto || 3000, () =>{
     console.log('Servidor corriendo en el puerto 3000')
 });
 
