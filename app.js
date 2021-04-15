e0e54b5d0b1510fed099de9b95f3f0a79e3c8625
IvanRebora <ivan.rebora@hotmail.com>
2021-04-15 15:14:31 -0300

    Agregando ejs

--- app.js
+++ app.js
@@ -7,18 +7,20 @@ const puerto = process.env.PORT
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
+app.set('view engine', 'ejs');
+
 app.listen(puerto || 3000, () =>{
     console.log('Servidor corriendo en el puerto 3000')
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
-})
+    res.render('home');
+});
 
 app.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/register.html'));
-})
+    res.render('register');
+});
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/login.html'));
-})
+    res.render('login');
+});
